9aeec0ca1aaf439591560a5300495b56c15bd6ee
caarlosdamian <dapecarlos@gmail.com>
2021-07-15 22:36:29 -0500

    change class components to functional components add useeffect

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,36 @@
-import React from "react";
-import "../styles/Custom.css";
+import React, { useState, useEffect } from 'react'
+import '../styles/Custom.css';
 
-const Card = ({ name, title, avatar, onDelete, onChangeName }) => {
-  return (
-    <div>
-      <div className="card">
-        <img src={avatar} alt="Avatar" style={{ width: "100%" }} />
-        <div className="container">
-          <h4>
-            <b>{name}</b>
-          </h4>
-          <p>{title}</p>
-          <input type="text" value={name} onChange={onChangeName}></input>
-          <p>
-            <button className="button button-red " onClick={onDelete}>
-              Delete
-            </button>
-          </p>
+const Card = props => {
+
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+    const recordMouse = e => {
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+    console.log('mouse event')
+    useEffect(() => {
+        window.addEventListener('mousemove', recordMouse)
+
+        return () => {
+            console.log('Cardjs cleanup')
+            window.removeEventListener('mousemove', recordMouse)
+        }
+    }, [])
+    return (
+        <div className="card">
+            <p>x position : {x} </p><p> y position: {y} </p>
+            <img src={props.avatar} alt="Avatar" style={{ width: '100%' }} />
+            <div className="container">
+                <h4><b>{props.name}</b></h4>
+                <p>{props.title}</p>
+                <input type="text" onChange={props.onChangeName} value={props.name} />
+                <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
+                <div>{props.children}</div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Card;
+export default Card

6a5c69318bd0eb07d487ef418149a442309b16c9
caarlosdamian <dapecarlos@gmail.com>
2021-07-15 23:14:54 -0500

    add axios to useeffect

--- src/App.js
+++ src/App.js
@@ -1,80 +1,81 @@
-import React, {  useState } from 'react';
-import './styles/Custom.css';
-import Card from './components/Card';
-import { ThemeProvider } from 'styled-components'
-import Button from './elemnt/Button'
-import Axios from 'axios'
+import React, { useState, useEffect } from "react";
+import "./styles/Custom.css";
+import Card from "./components/Card";
+import { ThemeProvider } from "styled-components";
+import Button from "./elemnt/Button";
+import axios from "axios";
 const theme = {
-  primary: '#4CAF50',
-  mango: 'yellow'
-}
+  primary: "#4CAF50",
+  mango: "yellow",
+};
 
 function App() {
+  const [card, setCard] = useState([]);
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setCard(res.data);
+      });
+  }, [id]);
 
-  const [cards, setCards] = useState([
-    {
-      id: 'asdfsafsdf',
-      name: 'Damaris Schuster',
-      title: 'International Operations Producer',
-      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/karthipanraj/128.jpg'
-    },
-    {
-      id: 'gfhdfhg',
-      name: 'Bartholome Dietrich',
-      title: 'Future Security Developer',
-      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/cicerobr/128.jpg'
-    },
-    {
-      id: 'tbdfghfghf',
-      name: 'Pattie Miller',
-      title: 'Forward Metrics AnalystChange Name',
-      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/illyzoren/128.jpg'
-    }
-  ])
+  const [showCard, setShowCard] = useState(true);
 
-  const [showCard, setShowCard] = useState(true)
-
-  const toggleShowCard = () => setShowCard(!showCard)
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards]
-    cards_copy.splice(cardIndex, 1)
-    console.log('cards_copy', cards_copy)
-    console.log('cards', cards)
-    setCards(cards_copy)
-
-  }
+  const toggleShowCard = () => setShowCard(!showCard);
+  // const deleteCardHandler = (cardIndex) => {
+  //   const cards_copy = [...cards];
+  //   cards_copy.splice(cardIndex, 1);
+  //   setCards(cards_copy);
+  // };
   const changeNameHandler = (event, id) => {
     //1. which card
-    const cardIndex = cards.findIndex(card => card.id === id)
+
     //2. make a copy of the cards
-    const cards_copy = [...cards]
+    const card_copy = { ...card };
     //3. change the name of the specific card
-    cards_copy[cardIndex].name = event.target.value
+    card_copy.name = event.target.value;
     //4. set the cards with the latest version of card copy
-    setCards(cards_copy)
-  }
+    setCard(card_copy);
+  };
   // const buttonStyle = {
   //   backgroundColor: null
   // }
-  const classes = ['button']
-  if (cards.length < 3) classes.push('pink')
-  if (cards.length < 2) classes.push('red text');
+  // const classes = ["button"];
+  // if (card.length < 3) classes.push("pink");
+  // if (card.length < 2) classes.push("red text");
   const cardsMarkup = showCard && (
-    cards.map((card, index) => <Card
+    <Card
       avatar={card.avatar}
       name={card.name}
-      title={card.title}
+      phone={card.phone}
       key={card.id}
-      onDelete={() => deleteCardHandler(index)}
+      // onDelete={() => deleteCardHandler(index)}
       onChangeName={(event) => changeNameHandler(event, card.id)}
-    />)
-  )
+    />
+  );
+  // card.map((card, index) => (
+  //   <Card
+  //     avatar={card.avatar}
+  //     name={card.name}
+  //     phone={card.phone}
+  //     key={card.id}
+  //     // onDelete={() => deleteCardHandler(index)}
+  //     onChangeName={(event) => changeNameHandler(event, card.id)}
+  //   />
+  // ));
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Button color="mango" length={cards.length}>Toggle</Button>
-        <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle show/hide</button>
+        <input type='text' value={id} onChange={e=>setId(e.target.value)}></input>
+        <Button color="mango" length={card.length}>
+          Toggle
+        </Button>
+        <button className="button" onClick={toggleShowCard}>
+          Toggle show/hide
+        </button>
         {cardsMarkup}
       </div>
     </ThemeProvider>

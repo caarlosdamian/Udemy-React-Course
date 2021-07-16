8b86d46267e0540bee4fe14ffcd4e76d3e5b7ee7
caarlosdamian <dapecarlos@gmail.com>
2021-07-16 18:22:07 -0500

    useReducer+useeffect

--- lesson-2/lesson-2.2/src/App.js
+++ lesson-2/lesson-2.2/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import { Badge, ListGroup, ListGroupItem } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const initialState = {
+  loading: true,
+  error: "",
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_DATA":
+      return {
+        loading: false,
+        error: "",
+        todos: action.payload,
+      };
+    case "SET_ERROR":
+      return {
+        loading: false,
+        error: "There are some errors",
+        todos: [],
+      };
+    default:
+  }
+  return;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        dispatch({ type: "SET_DATA", payload: res.data });
+      })
+      .catch((err) => {
+        dispatch({ type: "SET_ERROR" });
+      });
+  }, []);
+  const liskmarkup = (
+    <ListGroup>
+      {state.todos.map((todo) => (
+        <ListGroupItem key={todo.id}>
+          {todo.title}
+          {todo.completed ? (
+            <Badge style={{backgroundColor:"#28a745"}}>Completed</Badge>
+          ) : (
+            <Badge style={{backgroundColor:"#dc3545"}}>Incompleted</Badge>
+          )}
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.loading ? "Loading" : state.error ? (  <Badge style={{backgroundColor:"red"}}>{state.error}</Badge>) : liskmarkup}
     </div>
   );
 }
